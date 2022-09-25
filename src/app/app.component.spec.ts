170432fd7bc8564d15e7df8295040308ca242e38
Elizabeth Pedraza <eli456pg@gmail.com>
2022-09-25 13:01:29 -0500

    unit testing trainning #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent Test Suit', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
